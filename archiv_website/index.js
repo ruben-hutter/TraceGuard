bca5d06b086971d17ffc2ad1cddc2adb0bc2aa9a
ruben-hutter <ruben.hutter@rubenhutter.ch>
2025-07-08 13:46:10 +0200

    Add archiv_website folder

---
+++ archiv_website/index.js
@@ -0,0 +1,43 @@
+function loadAbstract()
+{
+	document.getElementById("abstract").style.display="block";
+	document.getElementById("thesis").style.display="none";
+	document.getElementById("software").style.display="none";
+	document.getElementById("documentation").style.display="none";
+}
+
+function loadThesis()
+{
+	document.getElementById("abstract").style.display="none";
+	document.getElementById("thesis").style.display="block";
+	document.getElementById("software").style.display="none";
+	document.getElementById("documentation").style.display="none";
+}
+
+function loadSoftware()
+{
+	document.getElementById("abstract").style.display="none";
+	document.getElementById("thesis").style.display="none";
+	document.getElementById("software").style.display="block";
+	document.getElementById("documentation").style.display="none";
+}
+
+function loadDocumentation()
+{
+	document.getElementById("abstract").style.display="none";
+	document.getElementById("thesis").style.display="none";
+	document.getElementById("software").style.display="none";
+	document.getElementById("documentation").style.display="block";
+}
+
+function mouseOver(button)
+{
+	button.style.background = '#000000';
+	button.style.color = '#FFFFFF';
+}
+
+function mouseOut(button)
+{
+	button.style.background = '#999999';
+	button.style.color = '#000000';
+}
